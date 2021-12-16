928e3d5b3d3808deac5f1e836817150d1a80f35f
Abhijeet Rana <82932131+TheMainManIND@users.noreply.github.com>
2021-12-16 20:18:05 +0530

    Add files via upload

---
+++ SumOfDigits.java
@@ -0,0 +1,16 @@
+package com.abhijeet.Recursion.Basic;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+
+        System.out.println(sumOfDigits(789421));
+    }
+
+    static int sumOfDigits(int n){
+
+        if (n == 0){
+            return 0;
+        }
+        return (n % 10) + sumOfDigits(n/10);
+    }
+}
