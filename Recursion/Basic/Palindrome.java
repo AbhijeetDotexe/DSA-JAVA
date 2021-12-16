0339b0f69b2338cd7ee4678596a676662cf304aa
Abhijeet Rana <82932131+TheMainManIND@users.noreply.github.com>
2021-12-16 20:17:42 +0530

    Add files via upload

---
+++ Recursion/Basic/Palindrome.java
@@ -0,0 +1,24 @@
+package com.abhijeet.Recursion.Basic;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+        System.out.println(palindrome(333));;
+    }
+
+    static boolean palindrome(int n){
+       return n == reverse(n);
+    }
+
+    static int sum = 0;
+    static int reverse(int n){
+        if (n == 0){
+            return 0 ;
+        }
+        int rem = n % 10;
+        sum = sum * 10 + rem;
+        reverse(n/10);
+        return sum;
+    }
+
+}
